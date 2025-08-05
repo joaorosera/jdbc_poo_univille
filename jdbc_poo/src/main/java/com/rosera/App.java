76cde36004eb311ac326ca645c1eea8db8bfaf84
João Vitor Rosera <jvrosera01@gmail.com>
2025-08-04 21:49:22 -0300

    Update App.java

--- jdbc_poo/src/main/java/com/rosera/App.java
+++ jdbc_poo/src/main/java/com/rosera/App.java
@@ -1,22 +1,25 @@
 package com.rosera;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
-public class App
-{
+public class App {
     public static void main(String[] args) {
         String url = "jdbc:mysql://localhost:3306/jdbc_poo";
         String user = "root";
         String password = "univille";
+
         try {
             Connection con = DriverManager.getConnection(url, user, password);
             System.out.println("Conectou!");
-            Statement st = con.createStatement();
-            String sql = "insert into pessoa(nome,cpf) values('Dexter Morgan','000666000');";
-            st.execute(sql);
+
+            String sql = "INSERT INTO pessoa(nome, cpf) VALUES (?, ?)";
+            PreparedStatement pre = con.prepareStatement(sql);
+            pre.setString(1, "Tom");
+            pre.setString(2, "1231232123");
+            pre.execute();
+
+            System.out.println("Dados inseridos com sucesso!");
+
         } catch (SQLException e) {
             System.out.println("Deu erro!");
             e.printStackTrace();
